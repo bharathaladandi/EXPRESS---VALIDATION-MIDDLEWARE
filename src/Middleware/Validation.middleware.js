7a616e38652049272dd5cb3bcd82f4288647aa8d
bharathaladandi <bharathaladandi@gmail.com>
2022-10-14 21:28:31 +0530

    Done

---
+++ src/Middleware/Validation.middleware.js
@@ -0,0 +1,18 @@
+const Validation = (req, res, next) =>{
+
+    try{
+       if(id === typeof(number) && Name === typeof(string) && rating === typeof(number) &&
+        des === typeof(string) && genre === typeof(string) && cast === typeof(Array(string))) {
+           next();
+       }
+    }
+    catch{
+        res.status(400).send("Data is incorrect")
+    }
+
+
+}
+
+
+
+module.exports = Validation;
